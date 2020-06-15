10ed9b1b7fffcfdc069273a36fe6a6645855ba60
Mark Heydon <markheydon@mhcg.co.uk>
2020-06-15 21:22:06 +0100

    First stab at Azure function to take Work Log from Jira via webhook and add to Zoho People.

---
+++ JiraToPeople.AzFunctions/JiraWorklogToPeople.cs
@@ -0,0 +1,107 @@
+// Copyright (c) 2020 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#nullable enable
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using JiraToPeople.PeopleAPI.Model;
+using System.Linq;
+using System;
+using JiraToPeople.PeopleAPI;
+
+namespace JiraToPeople.AzFunctions
+{
+    public static class JiraWorklogToPeople
+    {
+        internal static readonly ZohoPeopleHttpClient PeopleHttpClient
+            = new ZohoPeopleHttpClient(Environment.GetEnvironmentVariable("ZOHO_PEOPLE_API_AUTHTOKEN")!);
+
+        [FunctionName("JiraWorklogToPeople")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("JiraWorklogToPeople started.");
+
+            // Take values passed in from Jira webhook and call the
+            // Zoho People Time Log API to add new record.
+            string worklogStarted = req.Query["worklogStarted"].FirstOrDefault();
+            string worklogTimeSpentSeconds = req.Query["worklogTimeSpentSeconds"].FirstOrDefault();
+            string worklogUserEmail = req.Query["worklogUserEmail"].FirstOrDefault();
+            string worklogJobID = req.Query["worklogJobID"].FirstOrDefault();
+
+            string worklogComment = req.Query["worklogComment"].FirstOrDefault();
+            string issueSummary = req.Query["issueSummary"].FirstOrDefault();
+            string projectSummary = req.Query["projectSummary"].FirstOrDefault();
+
+#if DEBUG
+            log.LogDebug("worklogStarted", worklogStarted);
+            log.LogDebug("worklogTimeSpentSeconds", worklogTimeSpentSeconds);
+            log.LogDebug("worklogUserEmail", worklogUserEmail);
+            log.LogDebug("worklogJobID", worklogJobID);
+            log.LogDebug("worklogComment", worklogComment);
+            log.LogDebug("issueSummary", issueSummary);
+            log.LogDebug("projectSummary", projectSummary);
+#endif
+
+            // Format the TimeLog entry as required.
+            PeopleTimeLog? timelog = PeopleTimeLog.CreateFromStrings(
+                userEmail: worklogUserEmail,
+                jobID: worklogJobID,
+                startDateTime: worklogStarted,
+                durationInSeconds: worklogTimeSpentSeconds
+                );
+
+            var result = new { timeLogId = "" };
+            if (timelog != null)
+            {
+                timelog.WorkItem = $"{projectSummary}: {issueSummary}";
+                timelog.Description = !string.IsNullOrWhiteSpace(worklogComment) ? worklogComment : timelog.WorkItem;
+
+                log.LogInformation("Timelog created from query string parameters.", timelog);
+                string logID = "";
+                try
+                {
+                    logID = await PeopleHttpClient.AddTimeLogAsync(timelog);
+                    log.LogInformation("AddtimeLogAsync called without issues.", logID);
+                    result = new { timeLogId = logID.ToString() };
+                }
+                catch (Exception ex)
+                {
+                    log.LogCritical("Unable to call AddTimeLogAsync.", ex);
+                    throw ex;
+                }
+            }
+            else
+            {
+                log.LogError("Unable to create timelog object from specified parameters.");
+                throw new Exception();
+            }
+
+            log.LogInformation("JiraWorklogToPeople finished.");
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ JiraToPeople.Tests/ModelTests.cs
@@ -0,0 +1,191 @@
+// Copyright (c) 2020 
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using Xunit;
+using JiraToPeople.PeopleAPI.Model;
+
+namespace JiraToPeople.Tests
+{
+    public class ModelTests
+    {
+        #region Some known stuff for testing.
+
+        internal const string TestUserEmail = @"whatev@example.com";
+        internal const string TestJobID = @"111";
+
+        internal const int OneHourInSeconds = 3600;
+        internal const int OneAndHalfHoursInSeconds = 5400;
+
+        internal static TimeSpan OneHourInTimeSpan => new TimeSpan(1, 0, 0);
+        internal static TimeSpan OneAndHalfHoursTimeSpan => new TimeSpan(1, 30, 0);
+
+
+        #endregion
+
+        #region Type tests.
+
+        [Fact]
+        public void TypeSeconds()
+        {
+            var timeLog = new PeopleTimeLog(TestUserEmail, TestJobID, DateTime.Now, OneHourInSeconds);
+            Assert.IsType<PeopleTimeLog>(timeLog);
+        }
+
+        [Fact]
+        public void TypeTimeSpan()
+        {
+            var timeLog = new PeopleTimeLog(TestUserEmail, TestJobID, DateTime.Now, OneHourInTimeSpan);
+            Assert.IsType<PeopleTimeLog>(timeLog);
+        }
+
+        #endregion
+
+        #region Date tests.
+
+        [Fact]
+        public void StartDateTest()
+        {
+            var timeNow = DateTime.Now;
+            var timeLog = new PeopleTimeLog(TestUserEmail, TestJobID, timeNow, OneHourInSeconds);
+
+            Assert.Equal<DateTime>(timeNow, timeLog.StartDateTime);
+            Assert.Equal<DateTime>(timeNow.Date, timeLog.WorkDate);
+        }
+
+        [Fact]
+        public void EndDateTestOneHour()
+        {
+            var startDateTime = new DateTime(2012, 1, 1, 10, 0, 0);
+            var expected = new DateTime(2012, 1, 1, 11, 0, 0);
+
+            var timeLog = new PeopleTimeLog(TestUserEmail, TestJobID, startDateTime, OneHourInSeconds);
+            Assert.Equal<DateTime>(expected, timeLog.EndDateTime);
+        }
+
+        [Fact]
+        public void EndDateTestOneAndHalfHours()
+        {
+            var startDateTime = new DateTime(2012, 1, 1, 10, 0, 0);
+            var expected = new DateTime(2012, 1, 1, 11, 30, 0);
+
+            var timeLog = new PeopleTimeLog(TestUserEmail, TestJobID, startDateTime, OneAndHalfHoursInSeconds);
+            Assert.Equal<DateTime>(expected, timeLog.EndDateTime);
+        }
+
+        #endregion
+
+        #region BillingStatusType tests.
+
+        [Fact]
+        public void BillingStatusBillable()
+        {
+            var timelog = new PeopleTimeLog(TestUserEmail, TestJobID, DateTime.Now, OneAndHalfHoursInSeconds)
+            {
+                BillingStatus = PeopleTimeLog.BillingStatusType.Billable
+            };
+            Assert.Equal("billable", timelog.BillingStatus.ConvertToString());
+        }
+
+        [Fact]
+        public void BillingStatusNonBillable()
+        {
+            var timelog = new PeopleTimeLog(TestUserEmail, TestJobID, DateTime.Now, OneAndHalfHoursInSeconds)
+            {
+                BillingStatus = PeopleTimeLog.BillingStatusType.NonBillable
+            };
+            Assert.Equal("non-billable", timelog.BillingStatus.ConvertToString());
+        }
+
+        #endregion
+
+        #region String convert tests.
+
+        [Fact]
+        public void ConvertStringsInvalid()
+        {
+            var invalid = PeopleTimeLog.CreateFromStrings(null, null, null, null);
+            Assert.Null(invalid);
+        }
+
+        [Fact]
+        public void ConvertStringsInvalidJobID()
+        {
+            var invalid = PeopleTimeLog.CreateFromStrings(
+                userEmail: TestUserEmail,
+                jobID: null,
+                DateTime.Now.ToString(),
+                OneHourInSeconds.ToString()
+                );
+            Assert.Null(invalid);
+        }
+
+        [Fact]
+        public void ConvertStringsInvalidStartDateTime()
+        {
+            var invalid = PeopleTimeLog.CreateFromStrings(
+                userEmail: TestUserEmail,
+                TestJobID.ToString(),
+                startDateTime: null,
+                OneHourInSeconds.ToString()
+                );
+            Assert.Null(invalid);
+        }
+
+        [Fact]
+        public void ConvertStringsInvalidDuration()
+        {
+            var invalid = PeopleTimeLog.CreateFromStrings(
+                userEmail: TestUserEmail,
+                TestJobID.ToString(),
+                DateTime.Now.ToString(),
+                durationInSeconds: null
+                );
+            Assert.Null(invalid);
+        }
+
+        [Fact]
+        public void ConvertStringsInvalidEmail()
+        {
+            var invalid = PeopleTimeLog.CreateFromStrings(
+                userEmail: null,
+                TestJobID.ToString(),
+                DateTime.Now.ToString(),
+                OneHourInSeconds.ToString()
+                );
+            Assert.Null(invalid);
+        }
+
+        [Fact]
+        public void ConvertStringsValid()
+        {
+            var valid = PeopleTimeLog.CreateFromStrings(
+                TestUserEmail,
+                TestJobID.ToString(),
+                DateTime.Now.ToString(),
+                OneHourInSeconds.ToString()
+                );
+            Assert.NotNull(valid);
+            Assert.IsType<PeopleTimeLog>(valid);
+        }
+
+        #endregion
+    }
+}
